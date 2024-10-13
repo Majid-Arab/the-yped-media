a9e8623f7adfd87141b2f28613d9a686448892c0
Majid-Arab <arabmajid12@gmail.com>
2024-10-14 00:53:37 +0500

    created advantage section

---
+++ src/components/AdvantagesSection.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import {
+    Card,
+    Text,
+    SimpleGrid,
+    UnstyledButton,
+    Anchor,
+    Group,
+    useMantineTheme,
+} from '@mantine/core';
+import {
+    IconCreditCard,
+    IconBuildingBank,
+    IconRepeat,
+    IconReceiptRefund,
+    IconReceipt,
+    IconReceiptTax,
+    IconReport,
+    IconCashBanknote,
+    IconCoin,
+} from '@tabler/icons-react';
+import classes from './AdvantagesSection.module.css';
+
+const mockdata = [
+    { title: 'Portfolio Website', icon: IconCreditCard, color: 'violet' },
+    { title: '3D Website', icon: IconBuildingBank, color: 'indigo' },
+    { title: 'UI / UX', icon: IconRepeat, color: 'blue' },
+    { title: 'Interactive Website', icon: IconReceiptRefund, color: 'green' },
+    { title: 'landing pages', icon: IconReceipt, color: 'teal' },
+    { title: 'Agency Website', icon: IconReceiptTax, color: 'cyan' },
+    { title: 'Product Website', icon: IconReport, color: 'pink' },
+    { title: 'Ecommerce', icon: IconCoin, color: 'red' },
+];
+
+export function AdvantagesSection() {
+    const theme = useMantineTheme();
+
+    const items = mockdata.map((item) => (
+        <UnstyledButton key={item.title} className={classes.item}>
+            <item.icon color={theme.colors[item.color][6]} size="6rem" />
+            <Text size="xs" mt={7} className={classes.text}>
+                {item.title}
+            </Text>
+        </UnstyledButton>
+    ));
+
+    return (
+        <Card withBorder radius="md" className={classes.card}>
+            <Group mb={50}>
+                <Text className={classes.title}>Project Types we Specialize in</Text>
+            </Group>
+            <SimpleGrid cols={3} mt="md" spacing="xl" verticalSpacing="xl">
+                {items}
+            </SimpleGrid>
+        </Card>
+    );
+}
