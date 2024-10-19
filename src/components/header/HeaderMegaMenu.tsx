6ef049e5589afb7e342b24bb2b422adea41136b4
Majid-Arab <arabmajid12@gmail.com>
2024-10-19 10:51:00 +0500

    header responsiveness

--- src/components/header/HeaderMegaMenu.tsx
+++ src/components/header/HeaderMegaMenu.tsx
@@ -1,35 +1,31 @@
-
-"use client"
-import {
-  Group,
-  Button,
-  Box,
-} from "@mantine/core";
+"use client";
+import { Group, Button, Box } from "@mantine/core";
 import classes from "./HeaderMegaMenu.module.css";
 
 export function HeaderMegaMenu() {
-
-  // Function to handle scroll
   const handleScrollToContact = () => {
-    const contactSection = document.getElementById('contact');
+    const contactSection = document.getElementById("contact");
     if (contactSection) {
-      contactSection.scrollIntoView({ behavior: 'smooth' });
+      contactSection.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   return (
-    <Box pb={120}>
+    <Box>
       <header className={classes.header}>
-        <Group justify="space-between" h="100%">
+        <Group justify="space-between" w="100%">
           <a href="#" className={classes.link}>
             The Typed Media
             {/* The <span style={{ fontFamily: "monospace", }}>Typed Media</span> */}
           </a>
 
-          <Group visibleFrom="sm">
-            <Button variant="default" onClick={handleScrollToContact}>Contact</Button>
-          </Group>
-
+          <Button
+            variant="default"
+            className={classes.btn}
+            onClick={handleScrollToContact}
+          >
+            Contact
+          </Button>
         </Group>
       </header>
     </Box>
